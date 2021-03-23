e41a3da925d77cddc94577cb26c69bfae25f4d38
FernB <fernbradder@gmail.com>
2021-03-23 14:24:46 +0800

    create and print txt file of analysis

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,21 +3,25 @@ import csv
 
 csvpath = os.path.join('Resources','budget_data.csv')
 
-csvdic = []
+csvlist = []
 with open(csvpath) as csvfile:
     reader = csv.reader(csvfile, delimiter = ",")
     next(reader)
     
     
     for row in reader:
-        csvdic.append(row)
+        csvlist.append(row)
     
-    print(len(csvdic))
 
-
-    total = [int(i[1]) for i in csvdic]
-    print(sum(total))
-
+totalmonths = len(csvlist)
+total = sum([int(i[1]) for i in csvlist])
+averagechange = int(total/totalmonths)
+GreatestInc = max(csvlist, key=lambda x: x[1])
+GreatestDec = min(csvlist, key=lambda x: x[1])
+File = open("Financial_Analysis.txt","r+")
+print(f"Financial Analysis\n---------------------------\nTotal Months: {totalmonths}\nTotal: ${total}\nAverage Change: ${averagechange}\nGreatest Increase in Profits: {GreatestInc[0]} (${GreatestInc[1]})\nGreatest Decrease in Profits: {GreatestDec[0]} (${GreatestDec[1]})", file = File)
+File.seek(0)
+print(File.read())
 
 
 
